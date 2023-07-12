a29e58ff83f70f8ba08a3c45cff4bbcd8b09322d
Paulina Habbecke <122784636+PaulinaHa1986@users.noreply.github.com>
2023-07-12 17:03:41 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+const question1 = (myNumber) => {
+    let myDept;
+    if (myNumber >=1) {"Informatics"
+
+}
+return myDepth;
+};
+
+const question2 = (myValue) => {
+    let myLang;
+    if(myValue.lenght ===10)
+    {myLang = "JavaScript";}
+    else{myLang = "Python";}
+    return myLang;
+};
+
+const question3 = (myValue) => {
+    let myLang;
+    if(myValue.lenght===10)
+    {myLang = "JavaScript";}
+    else {myLang = "Python";}
+    if(myValue.lenght===10)
+    {myLang= "JavaScript";}
+    else if (myValue.lenght>3)
+    {myLang = "PHP";}
+    else{myLang = "Python";.}
+    return myLang;
+};
