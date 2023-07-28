5a358174a7c395c2b8aa8d9b7434a21e6ac32362
Paulina Habbecke <122784636+PaulinaHa1986@users.noreply.github.com>
2023-07-28 15:45:07 +0200

    Add files via upload

---
+++ modyfied.js
@@ -0,0 +1,44 @@
+const question1 = (myNumber) => {
+    let myDept;
+    if (myNumber >=1) {"Informatics"
+}
+return myDepth;
+};
+
+const question2 = (myValue) => {
+    let myLang;
+    if(myValue.lenght ===10)
+    {myLang = "JavaScript";}
+    else{myLang = "Python";}
+    return myLang;
+};
+const question3 =(myValue) => {
+let myLang;
+if(myValue.lenght===10)
+{myLang_ "Java Script";}
+else {myLang = "Python";}
+return myLang;
+};
+
+const question4 = (myNumber) =>{
+let myFaveFood;
+case '1': System.out.print("Pizza");
+break;
+case '2': System.out.print("Hamburger");
+break;
+case '3': System.out.print("Ice Cream");
+break;
+case '4': System.out.print("Sushi");
+break;
+default: System.out.print("Pho");
+return myFaveFood;
+};
+
+const question5 = (myTemp) => {
+    let myWeather;
+    if(myTemp >=75)
+    {myWeather = "Great Weather";}
+    else {myWeather = "Still cold";}
+    return myWeather;
+};
+
